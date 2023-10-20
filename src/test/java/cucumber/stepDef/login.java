24f06244d218452ca6f54cd30e3c068fbd68dec2
Firman <fblazt@outlook.com>
2023-10-20 22:10:02 +0700

    Cucumber test

--- src/test/java/cucumber/stepDef/login.java
+++ src/test/java/cucumber/stepDef/login.java
@@ -4,9 +4,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class login {
     WebDriver driver;
@@ -14,7 +18,9 @@ public class login {
 
     @Given("Login page Swag Labs")
     public void loginPageSwagLabs() {
+        WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get(baseUrl);
     }
 
@@ -35,13 +41,20 @@ public class login {
 
     @Then("User logged in")
     public void userLoggedIn() {
+        String productsText = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
+        Assert.assertEquals(productsText, "Products");
+        driver.close();
     }
 
     @And("Input Invalid Password")
     public void inputInvalidPassword() {
+        driver.findElement(By.id("password")).sendKeys("dummy_pass");
     }
 
     @Then("User get error message")
     public void userGetErrorMessage() {
+        String errorLogin = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+        Assert.assertEquals(errorLogin, "Epic sadface: Username and password do not match any user in this service");
+        driver.close();
     }
 }
