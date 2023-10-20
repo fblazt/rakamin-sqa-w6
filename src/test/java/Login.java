3b180c9f6fc60678978c6625cd7ad16c4472fa86
Firman <fblazt@outlook.com>
2023-10-20 08:58:42 +0700

    Success login test case

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,11 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Login {
     @Test
-    public void open_browser() {
+    public void success_login() {
         WebDriver driver;
         String baseUrl = "https://www.saucedemo.com";
 
@@ -14,5 +16,13 @@ public class Login {
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(baseUrl);
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        String logoutText = driver.findElement(By.id("logout_sidebar_link")).getText();
+        Assert.assertEquals(logoutText, "Logout");
     }
 }
